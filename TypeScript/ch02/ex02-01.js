10878d2bb964ba2a51bcc10343c194b962101a4f
zxxng <jeayeong123@naver.com>
2023-11-02 11:33:45 +0900

    231102(TS): DAY2 학습자료 업데이트

---
+++ TypeScript/ch02/ex02-01.js
@@ -0,0 +1,7 @@
+(()=>{
+  class HighSchool{
+    
+  }
+  var s1 = new HighSchool(100, 91);
+  console.log('평균', s1.avg());
+})();
---
+++ TypeScript/ch02/ex02-05/math.js
@@ -0,0 +1,11 @@
+function sum(data){
+  return data.a + data.b;
+}
+
+function substract(data){
+  return data.a - data.b;
+}
+
+function divide(data){
+  return data.a / data.b;
+}
